893320f08fb25c367e4df2c25759168dc96e3a83
Kseniya <zlaya_kobra@bk.ru>
2020-06-05 18:39:22 +0300

    Changed PostManager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,19 @@
+package manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] searchWall(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+
+        //TODO: add logic
+
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+
+        //TODO: add logic
+    }
+}
